f7004c2bd9fd5cd592340a694b6b2bf9798b7cd4
Yuri Dias <yuridiaspr@gmail.com>
2022-12-16 17:45:48 -0300

    fix: add exercise 04

---
+++ src/04.sql
@@ -0,0 +1,1 @@
+SELECT MAX(jobs.salary) AS "maximumSalary", roles.name AS "role" FROM jobs JOIN roles ON jobs."roleId" = roles.id WHERE jobs.active = true GROUP BY roles.name ORDER BY MAX(jobs.salary);
